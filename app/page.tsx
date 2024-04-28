be991a70ead4a4fac381cb6154a79d2e9ebda598
jad <jadkoneissi@gmail.com>
2024-04-28 19:28:49 +0300

    analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-
+import { Analytics } from "@vercel/analytics/next";
 export default function Home() {
   return (
-    <div className="flex gap-[10vw] items-center justify-center w-full h-screen">
-      <Button asChild className="px-[5vw] text-[1.5rem]">
-        <Link href={"/login"}>Login</Link>
-      </Button>
-      <Button asChild className="px-[5vw] text-[1.5rem]">
-        <Link href={"/signup"}>SignUp</Link>
-      </Button>
-    </div>
+    <>
+      <Analytics />
+      <div className="flex gap-[10vw] items-center justify-center w-full h-screen">
+        <Button asChild className="px-[5vw] text-[1.5rem]">
+          <Link href={"/login"}>Login</Link>
+        </Button>
+        <Button asChild className="px-[5vw] text-[1.5rem]">
+          <Link href={"/signup"}>SignUp</Link>
+        </Button>
+      </div>
+    </>
   );
 }
