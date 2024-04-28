b9f306a0c7e6ceb56ba49db0db4c0225a4dc3673
jad <jadkoneissi@gmail.com>
2024-04-28 19:14:17 +0300

    auth for subject

--- app/api/courses/subject/route.ts
+++ app/api/courses/subject/route.ts
@@ -1,7 +1,15 @@
 import { prisma } from "../../prisma";
-
+import * as jwt from "jsonwebtoken";
 export async function POST(req: Request) {
   try {
+    const authHeader = req.headers.get("Authorization");
+    const token = authHeader?.split(" ")[1];
+    if (!token) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    if (!jwt.verify(token, process.env.SECRET!)) {
+      return new Response("Unauthorized", { status: 401 });
+    }
     const { subject } = await req.json();
     const courses = await prisma.course.findMany({
       where: {
