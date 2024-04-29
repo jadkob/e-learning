aa62c9eb90eda33ff6d85e3876e134bfda93b425
jad <jadkoneissi@gmail.com>
2024-04-29 12:27:59 +0300

    data

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -24,7 +24,7 @@ export default function Signup() {
       setCookie("token", res.data);
       router.push("/home");
     } catch (error: any) {
-      setMessage(error.message);
+      setMessage(error.response.data);
     } finally {
       setLoading(false);
     }
